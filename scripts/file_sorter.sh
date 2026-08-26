dc61483f36260efa465e51211d3d6aeb29bbf48c
amundgaard09 <nomispus@icloud.com>
2026-08-26 16:07:10 +0200

    .github folder

--- scripts/file_sorter.sh
+++ scripts/file_sorter.sh
@@ -3,13 +3,15 @@
 echo " Sorting files in the current directory..."
 
 # Create folders if they don't already exist
-mkdir -p Images Documents Code Binaries Configs JSONs 3D
+mkdir -p Images Videos Music Documents Scripts Binaries Configs JSONs 3D
 
 # Move files based on their extensions safely
 # The 2>/dev/null hides errors if there are no files of that type
 mv *.jpg *.png *.gif Images/ 2>/dev/null
+mv *.mp4 *.avi *.mov Videos/ 2>/dev/null
+mv *.mp3 *.wav *.ogg Music/ 2>/dev/null
 mv *.pdf *.txt *.docx Documents/ 2>/dev/null
-mv *.sh *.py *.html Code/ 2>/dev/null
+mv *.sh *.bash Scripts/ 2>/dev/null
 mv *.bin *.exe Binaries/ 2>/dev/null
 mv *.cfg *.conf Configs/ 2>/dev/null
 mv *.json JSONs/ 2>/dev/null
